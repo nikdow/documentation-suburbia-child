5a58649fcf5360a4c43c6765bebbde0ee79ab7d4
cbdweb <nik@cbdweb.net>
2020-05-01 16:54:46 +1000

    switch to Breadcrumb NavXT

--- single.php
+++ single.php
@@ -1,15 +1,15 @@
 <?php get_header(); ?>
-<div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
-    <?php
-    if(function_exists('bcn_display'))
-    {
-        bcn_display();
-    }?>
-</div>
+
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<?php include (get_stylesheet_directory() . "/meta.php"); ?>        
     <div id="single">
-
+        <div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
+            <?php
+            if(function_exists('bcn_display'))
+            {
+                bcn_display();
+            }?>
+        </div>
 		<h1><?php the_title(); ?></h1>
 		<?php
 		if ( has_post_thumbnail() ) :
