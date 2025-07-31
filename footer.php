ec78b27a1bb9b4240252ee2b4c8d9c93a7d302fe
nikdow <nik@nikdow.net>
2025-08-01 09:04:57 +1000

    update meeting address;

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 		</div>
 		<div class="footer clear">
         	<p>&copy; <?php the_time(__('Y')) ?> <?php bloginfo('name'); ?>. All Rights Reserved.</p>
-            <p class="about"><a href="https://calendar.app.google/t9VpiGnGqhvHkEZ47">Schedule a call or meeting</a></p>
+            <p class="about"><a href="https://www.cbdweb.net/contact-us/">Schedule a call or meeting</a></p>
         </div>
 	</div><!-- #wrapper -->
 <?php wp_footer(); ?>
